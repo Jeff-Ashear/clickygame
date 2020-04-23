f888203eae0a987f3536c591956acdf950b089df
Jeff.A <kinesthesiac@gmail.com>
2020-04-22 17:00:38 -0700

    Trying to figure out what each component should do and where the actual image files need to go.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import one from './images/one.JPG';
+import two from './images/two.JPG';
+import three from './images/three.JPG';
+import four from './images/four.JPG';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
   return (
+
     <div className="App">
+<nav className="navbar navbar-dark bg-dark">
+  <h1>Clikcy Game!</h1>
+  <h2>Score: x</h2>
+  <h2>Correct this round: y</h2>
+</nav>
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={one} className="clickImg" id="one" alt="One" />
+        <img src={two} className="clickImg" id="two" alt="two" />
+        <img src={three} className="clickImg" id="three" alt="three" />
+        <img src={four} className="clickImg" id="four" alt="four" />
       </header>
     </div>
+
   );
 }
 
