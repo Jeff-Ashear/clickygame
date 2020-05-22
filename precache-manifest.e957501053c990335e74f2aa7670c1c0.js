ae0b7be2f9b3a73c7c1771c1eb220c07e5ae95c6
Jeff.A <kinesthesiac@gmail.com>
2020-05-22 15:04:28 -0700

    Updates

---
+++ precache-manifest.e957501053c990335e74f2aa7670c1c0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80833dd59eb58a3015e00ca67d5ae190",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "73231c471581a78f3f25",
+    "url": "/clickygame/static/css/main.c1cfeb68.chunk.css"
+  },
+  {
+    "revision": "61f7a1a735d7cd32fec7",
+    "url": "/clickygame/static/js/2.dfce1598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.dfce1598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73231c471581a78f3f25",
+    "url": "/clickygame/static/js/main.cb275442.chunk.js"
+  },
+  {
+    "revision": "c798aa2c017665a2cf0e",
+    "url": "/clickygame/static/js/runtime-main.8918f080.js"
+  },
+  {
+    "revision": "b4dad1232adc92ebf24157b15180d1e7",
+    "url": "/clickygame/static/media/eight.b4dad123.jpg"
+  },
+  {
+    "revision": "ad93c3ff1cb1a2a3e5e53c18b75c2326",
+    "url": "/clickygame/static/media/eleven.ad93c3ff.jpg"
+  },
+  {
+    "revision": "a26ff1574e1d071113baacd73f10fef5",
+    "url": "/clickygame/static/media/five.a26ff157.jpg"
+  },
+  {
+    "revision": "618059c547d5bf5a6bf8950e1f54d879",
+    "url": "/clickygame/static/media/four.618059c5.jpg"
+  },
+  {
+    "revision": "7a6b1d4f1e7c122fec2f7b7e0c72d2b1",
+    "url": "/clickygame/static/media/nine.7a6b1d4f.jpg"
+  },
+  {
+    "revision": "62ac600bffbae2dacca25933e900aa84",
+    "url": "/clickygame/static/media/one.62ac600b.png"
+  },
+  {
+    "revision": "e7fb304663ad08e15306c5eb14027c2f",
+    "url": "/clickygame/static/media/seven.e7fb3046.jpg"
+  },
+  {
+    "revision": "2c9a327a3ab4b29eef3c75193644e555",
+    "url": "/clickygame/static/media/six.2c9a327a.jpg"
+  },
+  {
+    "revision": "4389958ea817fb667126ee73646ad274",
+    "url": "/clickygame/static/media/ten.4389958e.jpg"
+  },
+  {
+    "revision": "5598e90097b1b96d72b1e80ee6e383db",
+    "url": "/clickygame/static/media/three.5598e900.jpg"
+  },
+  {
+    "revision": "417366652aaff18011763e5e9439b129",
+    "url": "/clickygame/static/media/twelve.41736665.jpg"
+  },
+  {
+    "revision": "dd8c8823b73b7e3dd418c824f9509c7b",
+    "url": "/clickygame/static/media/two.dd8c8823.jpg"
+  }
+]);
